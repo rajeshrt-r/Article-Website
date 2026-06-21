db18adf1e01f257c05c1a3b831d82e81980186af
Rajesh.R <rajeshrt905@gmail.com>
2026-06-21 21:13:03 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'rks-tech-world-v1';
+const ASSETS = [
+  './RKS_TechWorld_Updated.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
