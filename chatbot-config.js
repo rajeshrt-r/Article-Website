b8993e90912038e9a6e1b67a6cdfe0bd1507162b
Rajesh.R <rajeshrt905@gmail.com>
2026-06-21 21:14:57 +0530

    Add files via upload

---
+++ chatbot-config.js
@@ -0,0 +1,7 @@
+// Chatbot feature flags – edit to enable/disable optional features
+export const CHATBOT_CONFIG = {
+  enableDarkMode: true,
+  enablePersistence: true,
+  enableStreaming: false,
+  enableCopyButton: true,
+};
